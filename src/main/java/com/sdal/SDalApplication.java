c2efc90b644febee7a9eb441ec5e54f8a2c8c31a
ksy <1218sss@naver.com>
2021-08-01 16:32:43 +0900

    술의 달인 프로젝트 시작
    - 술의 달인 틀 구현

---
+++ src/main/java/com/sdal/SDalApplication.java
@@ -0,0 +1,13 @@
+package com.sdal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SDalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SDalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdal/SDalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SDalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
